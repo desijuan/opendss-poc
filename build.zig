fd51f7645873f425af246c960071c7fb14153ac7
Juan Desimoni <juan.desimoni@codex.com.br>
2024-07-06 19:19:00 -0300

    Move to Zig v0.13.0

--- build.zig
+++ build.zig
@@ -2,23 +2,23 @@ const std = @import("std");
 const builtin = @import("builtin");
 
 pub fn build(b: *std.Build) void {
-    // Tested to work with Zig 0.12.0
-    if (builtin.zig_version.minor < 12)
-        @compileError("Please use Zig version 0.12 or higher");
+    // Tested with Zig v0.13.0
+    if (builtin.zig_version.minor < 13)
+        @compileError("Please use Zig version 0.13 or higher");
 
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
         .name = "opendss",
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
     exe.linkLibC();
-    exe.addIncludePath(.{ .path = "opendss" });
-    exe.addLibraryPath(.{ .path = "opendss" });
+    exe.addIncludePath(b.path("opendss"));
+    exe.addLibraryPath(b.path("opendss"));
     exe.linkSystemLibrary("dss_capi");
 
     b.installArtifact(exe);
